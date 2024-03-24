6f17b44e124aed82333d4749d73801a152510062
max <maxzapletin@gmail.com>
2024-03-24 21:02:29 +0200

    adding git ignore file

--- tasks_db.py
+++ tasks_db.py
@@ -9,10 +9,8 @@ def get_all_tasks():
         all_tasks = json.load(file)
         return all_tasks
 
-
-# def get_all_tasks():
-#     pass
-
+def get_specific_task():
+    
 
 # def add_task(task):
 #     pass
@@ -24,63 +22,3 @@ def get_all_tasks():
 
 # def update_task():
 #     pass
-
-
-# app = Flask("tasks")
-
-# all_tasks = [
-#     None,
-#     {'id': 1, 'title': 'shop', 'details': 'Milk'},
-#     {'id': 2, 'title': 'shop', 'details': 'Milk2'},
-#     {'id': 3, 'title': 'shop', 'details': 'Milk3'},
-#     {'id': 4, 'title': 'shop', 'details': 'Milk4'},
-# ]
-
-
-# @app.route("/tasks/")
-# def get_all_tasks():
-#     return json.dumps(all_tasks)
-
-
-# @app.route("/tasks/<int:task_id>")
-# def get_single_task(task_id):
-#     del all_tasks[int(task_id)]
-#     return json.dumps('deleted')
-
-
-# if __name__ == '__main__':
-#     app.run(port=5000)
-
-
-# app = Flask(__name__)
-
-# tasks = []
-
-
-# @app.route("/tasks", methods=["GET"])
-# def get_all_tasks():
-#     return json.dumps({'tasks': tasks})
-
-
-# @app.route("/tasks/<int:task_id>", methods=["GET"])
-# def get_task(task_id):
-#     for task in tasks:
-#         if task['id'] == task_id:
-#             return json.dumps(task)
-#     return json.dumps({"message": "Task not found"})
-
-
-# @app.route("/tasks", methods=["POST"])
-# def add_task():
-#     data = request.get_json()
-#     tasks_id = len(tasks) + 1
-#     task = {'id': tasks_id, 'shop': data['shop'], 'details': data['details']}
-#     tasks.append(task)
-#     return json.dumps({"message": f"Task number {tasks_id} was added"})
-
-# @app.route("/")
-# def delete():
-
-
-# if __name__ == '__main__':
-#     app.run(port=5000)
