f74cd15e227aeb45c8b6411474aa993168e4fe1a
max <maxzapletin@gmail.com>
2024-03-24 20:08:09 +0200

    Add new files and working enviroment

---
+++ tasks_1.py
@@ -0,0 +1,55 @@
+from flask import Flask, request
+import json
+import tasks_db
+
+app = Flask(__name__)
+
+# tasks = []
+
+
+@app.route("/tasks", methods=["GET"])
+def get_all_tasks():
+    #    return json.dumps({'tasks': tasks}),
+    all_tasks = tasks_db.get_all_tasks()
+    return json.dumps({'tasks': all_tasks})
+
+
+@app.route("/tasks/<int:task_id>", methods=["GET"])
+def get_task(task_id):
+    for task in tasks:
+        if task['id'] == task_id:
+            return json.dumps(task),
+    return json.dumps({"message": "Task not found"})
+
+
+@app.route("/tasks", methods=["POST"])
+def add_task():
+    data = request.get_json()
+    tasks_id = len(tasks_db) + 1
+    task = {'id': tasks_id, 'title': data['title'], 'details': data['details']}
+    tasks_db.append(task)
+    return json.dumps({"message": f"Task number {tasks_id} was added"}),
+
+
+# @app.route("/tasks/<int:task_id>", methods=["PUT"])
+# def update_task(task_id):
+#     data = request.get_json()
+#     for task in tasks:
+#         if task['id'] == task_id:
+#             task['title'] = data.get('title', task['title'])
+#             task['details'] = data.get('details', task['details'])
+#             return json.dumps({"message": f"Task number {task_id} was updated"}), 200
+#     return json.dumps({"message": "Task not found"}),
+
+
+# @app.route("/tasks/<int:task_id>", methods=["DELETE"])
+# def delete_task(task_id):
+#     for task in tasks:
+#         if task['id'] == task_id:
+#             tasks.remove(task)
+#             return json.dumps({"message": f"Task number {task_id} was deleted"}), 200
+#     return json.dumps({"message": "Task not found"}),
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
---
+++ tasks_db.py
@@ -0,0 +1,97 @@
+import json
+all_tasks_file = '/home/max/Study/task_api_server/tasks.json'
+
+# 1. Read file contents
+
+
+def get_all_tasks():
+    with open(all_tasks_file, "r") as file:
+        data = json.load(file)
+        return
+
+
+# entry = {'carl': 33}
+# # 1. Read file contents
+# with open(filename, "r") as file:
+#     data = json.load(file)
+# # 2. Update json object
+# data.append(entry)
+# # 3. Write json file
+# with open(filename, "w") as file:
+#     json.dump(data, file)
+
+
+# def get_all_tasks():
+#     pass
+
+
+# def add_task(task):
+#     pass
+
+
+# def delet_task(which_task):
+#     pass
+
+
+# def update_task():
+#     pass
+
+
+# app = Flask("tasks")
+
+# all_tasks = [
+#     None,
+#     {'id': 1, 'title': 'shop', 'details': 'Milk'},
+#     {'id': 2, 'title': 'shop', 'details': 'Milk2'},
+#     {'id': 3, 'title': 'shop', 'details': 'Milk3'},
+#     {'id': 4, 'title': 'shop', 'details': 'Milk4'},
+# ]
+
+
+# @app.route("/tasks/")
+# def get_all_tasks():
+#     return json.dumps(all_tasks)
+
+
+# @app.route("/tasks/<int:task_id>")
+# def get_single_task(task_id):
+#     del all_tasks[int(task_id)]
+#     return json.dumps('deleted')
+
+
+# if __name__ == '__main__':
+#     app.run(port=5000)
+
+
+# app = Flask(__name__)
+
+# tasks = []
+
+
+# @app.route("/tasks", methods=["GET"])
+# def get_all_tasks():
+#     return json.dumps({'tasks': tasks})
+
+
+# @app.route("/tasks/<int:task_id>", methods=["GET"])
+# def get_task(task_id):
+#     for task in tasks:
+#         if task['id'] == task_id:
+#             return json.dumps(task)
+#     return json.dumps({"message": "Task not found"})
+
+
+# @app.route("/tasks", methods=["POST"])
+# def add_task():
+#     data = request.get_json()
+#     tasks_id = len(tasks) + 1
+#     task = {'id': tasks_id, 'shop': data['shop'], 'details': data['details']}
+#     tasks.append(task)
+#     return json.dumps({"message": f"Task number {tasks_id} was added"})
+
+# @app.route("/")
+# def delete():
+
+
+# if __name__ == '__main__':
+#     app.run(port=5000)
